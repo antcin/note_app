ec5f701c3cf3123e20ee8691e8f28fab6aae0098
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2018-04-03 17:48:02 +0100

    html added for tests

--- spec/notebookSpec.js
+++ spec/notebookSpec.js
@@ -1,11 +1,48 @@
 'use strict';
 
-// (function(exports){
-  function testCreate(){
-    let notebook = new Notebook();
-    let testNote = 'hello, my name is Ricky and this is a cool test';
-    notebook.create(testNote);
-    return notebook.all.includes(testNote) ? true : false;
-  };
-  console.log(testCreate())
-// })(this);
+
+let tests =
+[
+  {
+    describe: '.create',
+    it: 'adds a string to notebook.all',
+    test: function() {
+      let notebook = new Notebook();
+      let testNote = 'hello, my name is Ricky and this is a cool test';
+      notebook.create(testNote);
+      return notebook.all.includes(testNote) ? true : false;
+    }
+  },
+  {
+   describe:
+   it: 'this is the second test',
+   test: function() {
+     let notebook = new Notebook();
+     let testNote = 'hello, my name is Ricky and this is a cool test';
+     notebook.create(testNote);
+     return notebook.all.includes(testNote) ? true : false;
+   }
+ }
+]
+
+
+function runAll(arrayOfTestObj) {
+  let element = document.getElementById('test_div')
+  let string = ''
+  console.log(string)
+  for (let i = 0;i < arrayOfTestObj.length;i++) {
+    string += testToHTML(arrayOfTestObj[i])
+    console.log(string)
+  }
+  element.innerHTML = string
+}
+
+function testToHTML(testObj) {
+  let resultColour = testObj.test() ? 'green' : 'red'
+  return `<p style="color:${resultColour}">${testObj.describe}:<br>${testObj.it}</p>`
+}
+
+runAll(tests)
+
+
+// console.log(testCreate())
