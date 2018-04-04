87102c2c5b6a821dedda62a4d6c9ab0b30fd4ec1
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2018-04-04 13:19:45 +0100

    clickme example

--- test.js
+++ test.js
@@ -12,5 +12,21 @@ function loadXMLDoc(file) {
   xhttp.send();
 }
 
-loadXMLDoc('jerryRunner.html')
-loadXMLDoc('src/notebook.js')
+function getScript(file) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var script = document.getElementById('imported_script')
+      script.innerHTML = this.responseText
+    }
+  };
+
+  xhttp.open("GET", file, true);
+  xhttp.send();
+}
+
+// loadXMLDoc('jerryRunner.html')
+// getScript('src/clickme.js')
+
+
+// var s = document.createElement('script')
